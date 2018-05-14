b25d0d478cfe0f6c9c473cf15ad79e81b20ac745
Rachel Westwater <rachel-westwater@hotmail.com>
2018-05-14 16:22:47 +0100

    Waterbottle classes passing successfully

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,2 +1,19 @@
 public class WaterBottle {
+    private int total;
+
+    public WaterBottle(int total){
+        this.total = total;
+    }
+
+    public int getDrink(){
+        return this.total -= 10;
+    }
+
+    public int getEmpty(){
+        return this.total = 0;
+    }
+
+    public int getFill(){
+        return this.total = 100;
+    }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+
+    WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void hasDrink(){
+        assertEquals(90, waterBottle.getDrink());
+    }
+
+    @Test
+    public void hasEmpty(){
+        assertEquals(0, waterBottle.getEmpty());
+    }
+
+    @Test
+    public void hasFill(){
+        assertEquals(100, waterBottle.getFill());
+    }
 }
