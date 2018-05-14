385efe1df47cdd16c3a3531468545ddc8340c652
Rachel Westwater <rachel-westwater@hotmail.com>
2018-05-14 16:56:38 +0100

    Printer Class Completed Successfully

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,36 @@
 public class Printer {
+    private int toner;
+    private int pages;
+    private int copies;
+
+    public Printer(int toner, int pages, int copies) {
+    this.toner = toner;
+    this.pages = pages;
+    this.copies = copies;
+    }
+
+    public int getPages(){
+        return this.pages;
+    }
+
+    public int getCopies(){
+        return this.copies;
+    }
+
+    public int getToner(){
+        return this.toner;
+    }
+
+    public int getRefill(){
+        return this.pages = 100;
+    }
+
+    public int getPrint() {
+        if (this.pages >= this.copies){
+            this.pages -= this.copies;
+            this.toner -= this.copies;
+        }
+        return this.pages;
+    }
+
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,45 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+
+    Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer(100, 100, 10);
+    }
+
+    @Test
+      public void hasPages(){
+        assertEquals(100, printer.getPages());
+    }
+
+    @Test
+    public void hasCopies(){
+        assertEquals(10, printer.getCopies());
+    }
+
+    @Test
+    public void hasToner(){
+        assertEquals(100, printer.getToner());
+    }
+
+    @Test
+    public void hasRefill(){
+        assertEquals(100, printer.getRefill());
+    }
+
+    @Test
+    public void hasPrint(){
+        printer.getPrint();
+        assertEquals(90, printer.getPages());
+        assertEquals(90, printer.getToner());
+    }
+
+
+
 }
+
