aabba26b5c85553eca082dfdd261fdde69c8133a
Rachel Westwater <rachel-westwater@hotmail.com>
2018-05-14 16:09:41 +0100

    Calculator Exercise Complete

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,26 +5,31 @@ import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
-    Calculator calculator
+    Calculator calculator;
 
     @Before
     public void before(){
-        calculator = new Calculator(4, 3);
+        calculator = new Calculator(4, 4);
     }
 
     @Test
     public void hasAdd(){
-        assertEquals(7, calculator.getAdd());
+        assertEquals(8, calculator.getAdd());
     }
 
     @Test
     public void hasSubtract(){
-        assertEquals(1, calculator.getSubtract());
+        assertEquals(0, calculator.getSubtract());
     }
 
     @Test
     public void hasDivide(){
-        assertEquals();
+        assertEquals(1, calculator.getDivide());
+    }
+
+    @Test
+    public void hasMultiply(){
+        assertEquals(16, calculator.getMultiply());
     }
 
 }
